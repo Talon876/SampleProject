b6a5254e758e219984cc7c2afdbe41a68d86155c
Steve Dighans <agentsdighans@gmail.com>
2012-04-13 19:30:12 -0600

    Added new SDApp class and added it to the call from App.java

--- src/main/java/com/deflexicon/sample/App.java
+++ src/main/java/com/deflexicon/sample/App.java
@@ -5,6 +5,8 @@ public class App
 	public App()
 	{
 		System.out.println("Hello World");
+		SDApp sdApp = new SDApp();
+		sdApp.doStuff();
 	}
 
 	public static void main(String[] args)
---
+++ src/main/java/com/deflexicon/sample/SDApp.java
@@ -0,0 +1,13 @@
+package com.deflexicon.sample;
+
+import java.util.Date;
+
+public class SDApp
+{
+
+	public void doStuff()
+	{
+		System.out.println("SD " + new Date().toString());
+	}
+	
+}
