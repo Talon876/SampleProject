96ff1d412a0037bc21f439af02e17fa158fcf58f
Talon Daniels <talondaniels@gmail.com>
2012-04-13 19:09:55 -0600

    Added files

---
+++ src/main/java/com/deflexicon/sample/App.java
@@ -0,0 +1,14 @@
+package com.deflexicon.sample;
+
+public class App
+{
+	public App()
+	{
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args)
+	{
+		new App();
+	}
+}
