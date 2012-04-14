cdd12a08110238d1dbb25169658976dc7cb35de7
Talon Daniels <talondaniels@gmail.com>
2012-04-14 16:27:34 -0600

    Added pieces to the Command framework and an ideas text file
    ideas.txt contains future ideas.
    COMMANDS is a public enum now
    HelpCommand was added, though not fully functional.

---
+++ src/main/java/com/deflexicon/sample/command/DataCommand.java
@@ -0,0 +1,11 @@
+package com.deflexicon.sample.command;
+
+/**
+ * Any command which will only be outputting data to the console should implement this interface.
+ * @author Talon
+ *
+ */
+public interface DataCommand
+{
+	public String getResults();
+}
---
+++ src/main/java/com/deflexicon/sample/command/commands/HelpCommand.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.deflexicon.sample.command.commands;
+
+import com.deflexicon.sample.command.COMMANDS;
+import com.deflexicon.sample.command.Command;
+import com.deflexicon.sample.command.DataCommand;
+
+/**
+ * @author Talon Daniels
+ *
+ */
+public class HelpCommand extends Command implements DataCommand
+{
+	String helpText = "Type help <command> for help with that command";
+	String output = "";
+
+	/**
+	 * Creates a Help command which can be executed
+	 */
+	public HelpCommand()
+	{
+		output = "Available Commands:\n";
+		for(COMMANDS cmd : COMMANDS.values())
+		{
+			output += cmd + "\n";
+		}
+	}
+	
+	public HelpCommand(String stringArg)
+	{
+		//TODO make this dynamicly get the help text for every command
+		if(stringArg.equalsIgnoreCase("help"))
+		{
+			output = new HelpCommand().getHelpText();
+		}
+	}
+
+	@Override
+	public String getResults()
+	{
+		return output;
+	}
+
+	@Override
+	public String getHelpText()
+	{
+		return helpText;
+	}
+
+}
