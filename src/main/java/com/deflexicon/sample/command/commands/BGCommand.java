b08e2e68400582354477531aa524cc8cdc4c35a1
Talon Daniels <talondaniels@gmail.com>
2012-04-14 21:17:28 -0600

    Rewording/Spelling

--- src/main/java/com/deflexicon/sample/command/commands/BGCommand.java
+++ src/main/java/com/deflexicon/sample/command/commands/BGCommand.java
@@ -8,7 +8,7 @@ public class BGCommand extends Command implements EffectCommand
 	@Override
 	public String getHelpText()
 	{
-		return "Draws a full sized rectangle in the color provided. Useful to set as the bottom later as anything drawn under this will not be seen.";
+		return "Draws a full sized rectangle in the color provided. Useful to set as the bottom layer as anything drawn under this will not be seen.";
 	}
 
 	@Override
